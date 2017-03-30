aa32063e355abc16932747612cd23cf09f704c74
Lauri <lauri.paatelainen@gmail.com>
2017-03-30 19:41:56 +0300

    Lisätty sql-lauseisiin cascade-säännöt

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -11,47 +11,47 @@ CREATE TABLE Ryhma(
     nimi            varchar(50)     NOT NULL,
     kuvaus          text            NOT NULL DEFAULT '',
     suljettu        boolean         NOT NULL DEFAULT FALSE,
-    yllapitaja      integer         REFERENCES Kayttaja(id)
+    yllapitaja      integer         REFERENCES Kayttaja(id) ON DELETE CASCADE
 );
 
 CREATE TABLE RyhmanJasenyys(
-    kayttaja        integer         REFERENCES Kayttaja(id),
-    ryhma           integer         REFERENCES Ryhma(id),
+    kayttaja        integer         REFERENCES Kayttaja(id) ON DELETE CASCADE,
+    ryhma           integer         REFERENCES Ryhma(id) ON DELETE CASCADE,
     PRIMARY KEY(kayttaja, ryhma)
 );
 
 CREATE TABLE Liittymispyynto(
-    kayttaja        integer         REFERENCES Kayttaja(id),
-    ryhma           integer         REFERENCES Ryhma(id),
+    kayttaja        integer         REFERENCES Kayttaja(id) ON DELETE CASCADE,
+    ryhma           integer         REFERENCES Ryhma(id) ON DELETE CASCADE,
     viesti          text            NOT NULL,
     PRIMARY KEY(kayttaja, ryhma)
 );
 
 CREATE TABLE Kutsu(
-    ryhma           integer         REFERENCES Ryhma(id),
-    kutsuttu        integer         REFERENCES Kayttaja(id),
+    ryhma           integer         REFERENCES Ryhma(id) ON DELETE CASCADE,
+    kutsuttu        integer         REFERENCES Kayttaja(id) ON DELETE CASCADE,
     viesti          text            NOT NULL,
     PRIMARY KEY(ryhma, kutsuttu)
 );
 
 CREATE TABLE Julkaisu(
     id              serial          PRIMARY KEY,
-    kayttaja        integer         REFERENCES Kayttaja(id) NOT NULL,
-    ryhma           integer         REFERENCES Ryhma(id),
+    kayttaja        integer         REFERENCES Kayttaja(id) ON DELETE CASCADE NOT NULL,
+    ryhma           integer         REFERENCES Ryhma(id) ON DELETE CASCADE,
     teksti          text            NOT NULL,
     aika            timestamp       NOT NULL
 );
 
 CREATE TABLE Tykkays(
-    kayttaja        integer         REFERENCES Kayttaja(id),
-    julkaisu        integer         REFERENCES Julkaisu(id),
+    kayttaja        integer         REFERENCES Kayttaja(id) ON DELETE CASCADE,
+    julkaisu        integer         REFERENCES Julkaisu(id) ON DELETE CASCADE,
     PRIMARY KEY(kayttaja, julkaisu)
 );
 
 CREATE TABLE Kommentti(
     id              serial          PRIMARY KEY,
-    kayttaja        integer         REFERENCES Kayttaja(id),
-    julkaisu        integer         REFERENCES Julkaisu(id),
+    kayttaja        integer         REFERENCES Kayttaja(id) ON DELETE CASCADE,
+    julkaisu        integer         REFERENCES Julkaisu(id) ON DELETE CASCADE,
     teksti          text            NOT NULL,
     aika            timestamp       NOT NULL
 );
