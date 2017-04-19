39950310c6a3207b2e26022b8010531ab2461f62
Lauri <lauri.paatelainen@gmail.com>
2017-04-19 18:50:51 +0300

    Ryhmän jäsenten listaus

--- app/controllers/ryhma_controller.php
+++ app/controllers/ryhma_controller.php
@@ -7,4 +7,10 @@ class RyhmaController extends BaseController {
         $ryhma = Ryhma::hae($id);
         View::make('groups/show.html', array('ryhma' => $ryhma));
     }
+    
+    public static function group_members($id) {
+        self::check_logged_in();
+        $ryhma = Ryhma::hae($id);
+        View::make('groups/members.html', array('ryhma' => $ryhma));
+    }
 }
